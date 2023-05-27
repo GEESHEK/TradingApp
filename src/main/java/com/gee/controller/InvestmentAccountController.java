3567d048f7c8e5ebd0c4e7835732df5564ad04a9
Gee Chong Shek <geeshek95@outlook.com>
2023-05-27 14:37:30 +0100

    Created getInvestmentAccount API which includes all the methods required in each layer.

--- src/main/java/com/gee/controller/InvestmentAccountController.java
+++ src/main/java/com/gee/controller/InvestmentAccountController.java
@@ -3,6 +3,7 @@ package com.gee.controller;
 import com.gee.model.InvestmentAccount;
 import com.gee.service.InvestmentAccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +19,14 @@ public class InvestmentAccountController {
         this.investmentAccountService = investmentAccountService;
     }
 
-    @GetMapping
+    @GetMapping //remove this late, should not be able to see everyone's accounts
     public List<InvestmentAccount> getInvestmentAccounts() {
-        return investmentAccountService.getInvestmentAccounts();
+        return investmentAccountService.getAllInvestmentAccounts();
+    }
+
+    @GetMapping("{investmentAccountId}")
+    public InvestmentAccount getInvestmentAccount(@PathVariable("investmentAccountId") Integer investmentAccountId) {
+        return investmentAccountService.getInvestmentAccount(investmentAccountId);
     }
 
 
--- src/main/java/com/gee/doa/InvestmentAccountDao.java
+++ src/main/java/com/gee/doa/InvestmentAccountDao.java
@@ -3,7 +3,9 @@ package com.gee.doa;
 import com.gee.model.InvestmentAccount;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface InvestmentAccountDAO {
+public interface InvestmentAccountDao {
     List<InvestmentAccount> selectAllInvestmentAccount();
+    Optional<InvestmentAccount> selectInvestmentAccountById(Integer id);
 }
